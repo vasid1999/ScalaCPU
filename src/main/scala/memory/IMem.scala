90e125d529e344458ec3da3cc7f86e31d97c6abe
vasid1999 <11539360+vasid1999@users.noreply.github.com>
2020-12-14 01:06:19 +0530

    edited IMem class

--- src/main/scala/memory/IMem.scala
+++ src/main/scala/memory/IMem.scala
@@ -6,6 +6,6 @@ class IMem extends Module{
 		val iaddr = Input(UInt(32.W))
 		val idata = Output(UInt(32.W))
 	})
-	val imem = SyncReadMem(1024,UInt(32.W))
+	val imem = Mem(1024,UInt(32.W))
 	io.idata := imem(io.iaddr)
 }
